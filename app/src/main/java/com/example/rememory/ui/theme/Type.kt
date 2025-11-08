b1f1f254fad6a5c04f554a9927e4b65e7530627f
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-09 00:44:51 +0900

    setting/#1: font 연결

--- app/src/main/java/com/example/rememory/ui/theme/Type.kt
+++ app/src/main/java/com/example/rememory/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.example.rememory.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.rememory.R
+
+val MontserratFontFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
