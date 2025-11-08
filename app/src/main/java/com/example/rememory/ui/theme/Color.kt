5cd4af7f4fad26fe94a1b3db2ef21e7d196b8bbb
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-09 01:01:36 +0900

    setting/#1: theme 설정

--- app/src/main/java/com/example/rememory/ui/theme/Color.kt
+++ app/src/main/java/com/example/rememory/ui/theme/Color.kt
@@ -18,11 +18,3 @@ val GrayText = Color(0xFF6B7280)
 
 // --- Font Colors ---
 val BlackText = Color(0xFF000000)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
