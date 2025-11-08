c63905061116d39135e80d64b1fce9f25c786744
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-09 00:35:24 +0900

    feat/#1: Color 설정

---
+++ app/src/main/java/com/example/rememory/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.rememory.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// --- Purple 계열 ---
+val PurplePrimary = Color(0xFFA855F7)
+val PurplePrimary50 = Color(0x80A855F7)
+val PurpleLight = Color(0xFFEAD5FD)
+val PurpleExtraLight = Color(0xFFF7EFFF)
+val PurpleText = Color(0xFF603886)
+
+// --- Background ---
+val BackgroundLight = Color(0xFFFAF5FF)
+
+// --- Gray 계열 ---
+val GrayBorder = Color(0xFFD1D5DB)
+val GrayText = Color(0xFF6B7280)
+
+// --- Font Colors ---
+val BlackText = Color(0xFF000000)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
