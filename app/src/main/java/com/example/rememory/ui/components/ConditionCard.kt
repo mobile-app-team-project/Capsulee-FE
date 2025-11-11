bc51291afb357aa01c4081cbc2114bf94c86db2b
bowoon1216 <happyanne200212@gmail.com>
2025-11-11 16:18:10 +0900

    chore: condition card 파일/초기세팅

---
+++ app/src/main/java/com/example/rememory/ui/components/ConditionCard.kt
@@ -0,0 +1,15 @@
+package com.example.rememory.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ConditionCard(){
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ConditionCardPreview(){
+
+}
