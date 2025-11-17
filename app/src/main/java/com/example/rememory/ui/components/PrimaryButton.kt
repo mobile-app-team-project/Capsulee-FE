5effd2a795e33a7533ea0168a04050eb5e75cfe2
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-18 00:28:12 +0900

    feat/#11: secondary 버튼 (테두리 버튼) 컴포넌트 제작

--- app/src/main/java/com/example/rememory/ui/components/PrimaryButton.kt
+++ app/src/main/java/com/example/rememory/ui/components/PrimaryButton.kt
@@ -1,11 +1,6 @@
 package com.example.rememory.ui.components
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
