7fb83f18ab5f6dedcf11811947655d9b96eb7d77
bowoon1216 <happyanne200212@gmail.com>
2025-11-29 14:16:07 +0900

    feat: 이미지 저장 및 기본틀

--- app/src/main/java/com/example/rememory/ui/screens/capsule/CapsuleListScreen.kt
+++ app/src/main/java/com/example/rememory/ui/screens/capsule/CapsuleListScreen.kt
@@ -1,2 +1,57 @@
 package com.example.rememory.ui.screens.capsule
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.rememory.R
+
+data class conditionInfo (
+    val type: String,
+    val value: String
+)
+data class CapsuleItemInfo (
+    val title: String,
+    val fromOrTo: String,
+    val opened: Boolean,
+    val conditionSummaries: List<conditionInfo>,
+)
+
+
+@Composable
+private fun CapsuleListItemCard(capsuleInfo: CapsuleItemInfo){
+    val iconRes =
+        if(capsuleInfo.opened){
+            R.drawable.ic_lock_opened
+        }else{
+            R.drawable.ic_lock_locked
+        }
+    Card {
+        Row {
+            Column {
+                Text(capsuleInfo.title)
+                Text(capsuleInfo.fromOrTo)
+            }
+            Image(
+                painter = painterResource(id = iconRes),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        Column {
+            capsuleInfo.conditionSummaries.forEach { item ->
+                Row {
+                    //타입에 따른 이미지 넣고
+                    Text(item.value)
+                }
+            }
+
+        }
+    }
+}
