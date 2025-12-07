bc068798a5ebeb2bcbe137ef66062a771a5412ca
wldy4627 <wldy4627@gmail.com>
2025-12-07 22:36:08 +0900

    feat/#26: 바텀바 없는 페이지 연결

--- app/src/main/java/com/example/rememory/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/rememory/ui/navigation/AppNavHost.kt
@@ -7,48 +7,63 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.compose.material3.Scaffold // Scaffold import
+import androidx.navigation.compose.rememberNavController
 import com.example.rememory.ui.components.BottomNavigationBar
+import com.example.rememory.ui.screens.capsuleCreate.CreateCapsuleFlow
 import com.example.rememory.ui.screens.capsuleList.CapsuleListScreen
-import com.example.rememory.ui.screens.friends.FriendManagingScreen
 
 @Composable
 fun AppNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    // 앱의 최상위 Scaffold
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route, // 캡슐 목록을 시작 화면으로 설정
+        modifier = modifier
+    ) {
+        // 온보딩 화면 (BottomBar 없음)
+//        composable(Screen.Onboarding.route) {
+//            OnboardingScreen(navController = navController)
+//        }
+
+        // 메인 앱 화면들 (BottomBar 포함)
+        composable(Screen.Home.route) {
+            MainScaffold(navController = navController)
+        }
+
+        composable(Screen.CapsuleCreate.route) {
+            CreateCapsuleFlow(navController = navController)
+        }
+    }
+}
+
+@Composable
+private fun MainScaffold(navController: NavHostController) {
+    val mainNavController = rememberNavController()
+
     Scaffold(
         bottomBar = {
-            BottomNavigationBar(navController = navController)
+            BottomNavigationBar(navController = mainNavController)
         }
     ) { innerPadding ->
-
-        // 실제 화면 전환 담당
         NavHost(
-            navController = navController,
-            startDestination = BottomNavItem.Capsule.route, // 캡슐 목록을 시작 화면으로 설정
-            modifier = modifier.padding(innerPadding)
+            navController = mainNavController,
+            startDestination = BottomNavItem.Capsule.route,
+            modifier = Modifier.padding(innerPadding)
         ) {
-            // ----------------------------------------------------
-            // 캡슐 목록 화면 정의
-            // ----------------------------------------------------
             composable(BottomNavItem.Capsule.route) {
-                CapsuleListScreen(
-                    navController = navController // 화면에 NavController 전달 (네비게이션 액션 사용 가능)
-                )
+                CapsuleListScreen(navController = mainNavController)
             }
 
-            // ----------------------------------------------------
-            // TODO: 다른 Bottom Bar 항목 및 화면을 여기에 정의합니다.
-            // ----------------------------------------------------
             composable(BottomNavItem.Home.route) {
-                // HomeScreen(navController = navController)
+//                 HomeScreen(navController = mainNavController)
             }
             composable(BottomNavItem.Friends.route) {
-                FriendManagingScreen()
+                // FriendsScreen(navController = mainNavController)
             }
             composable(BottomNavItem.MyPage.route) {
-                // MyPageScreen(navController = navController)
+                // MyPageScreen(navController = mainNavController)
             }
         }
     }
