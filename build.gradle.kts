e3cf6b3cbe4c8b1732009529201b272668fc5ec0
wldy4627 <wldy4627@gmail.com>
2025-12-08 02:45:09 +0900

    feat/#32: 구글맵 연동 및 gradle 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 
     //Hilt 플러그인
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.55" apply false
 }
